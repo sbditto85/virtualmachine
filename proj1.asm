59c5f9552c7f016c007295f9a03d3d42d8d9bd54
Casey Allred <sbditto85@gmail.com>
2013-09-27 21:37:00 -0600

    just getting setup

---
+++ proj1.asm
@@ -0,0 +1,21 @@
+#Print your name on the screen.
+#Print a blank line.
+#Add all the elements of list B together print each result (intermediate and final) on screen.  Put 2 spaces between each result. (e.g., 450)
+#Print a blank line.
+#Multiply all the elements of list A together print each result (intermediate and final) on screen. Put 2 spaces between each result. (e.g., 2)
+#Print a blank line
+#Divide the final result from part 3, by each element in list B (the results are not cumulative). Put 2 spaces between each result. (e.g., 1)
+#Print a blank line.
+#Subtract from the final result of part 5 each element of list C (the results are not cumulative). Put 2 spaces between each result. (e.g., 220)
+
+
+
+        LTRC    .BYT    'C'
+        LTRA    .BYT    'A'
+        LTRS    .BYT    'S'
+        LTRE    .BYT    'E'
+        LTRY    .BYT    'Y'
+        LTRSP   .BYT    ' '
+        LTRL    .BYT    'L'
+        LTRR    .BYT    'R'
+        LTRD    .BYT    'D'
