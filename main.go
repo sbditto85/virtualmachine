0b5d03712a2ca7fce3a10c78d780d6a2cb45c3a3
Casey Allred <sbditto85@gmail.com>
2013-10-10 22:14:34 -0600

    timing in each phase to see where the slow parts are

--- main.go
+++ main.go
@@ -6,18 +6,38 @@ import (
 	vm "./virtualmachine"
 	"flag"
 	"fmt"
+	"time"
 )
 
 func main() {
+	start := time.Now()
+	s := time.Now()
 	filename := flag.String("f", "", "the name of the .asm file to parse and run")
 	flag.Parse()
+	e := time.Now()
+	fmt.Printf("Flags: %v\n",e.Sub(s))
 
+	s = time.Now()
 	a := amb.NewAssembler()
+	e = time.Now()
+	fmt.Printf("NewAssembler: %v\n",e.Sub(s))
 
+	s = time.Now()
 	a.ReadFile(*filename)
+	e = time.Now()
+	fmt.Printf("ReadFile: %v\n",e.Sub(s))
+
+	s = time.Now()
 	fperr := a.FirstPass()
+	e = time.Now()
+	fmt.Printf("FirstPass: %v\n",e.Sub(s))
 	if fperr == nil {
+
+		s = time.Now()
 		sperr := a.SecondPass()
+		e = time.Now()
+		fmt.Printf("SecondPass: %v\n",e.Sub(s))
+
 		if sperr == nil {
 			sperr = sperr
 		} else {
@@ -43,9 +63,19 @@ func main() {
 	}*/
 	//fmt.Println(i)
 
+	s = time.Now()
 	v := vm.NewVirtualMachine(a.GetBytes())
+	e = time.Now()
+	fmt.Printf("NewVirtualMachine: %v\n",e.Sub(s))
+
+	s = time.Now()
 	verr := v.Run()
+	e = time.Now()
+	fmt.Printf("Run: %v\n",e.Sub(s))
 	if verr != nil {
 		fmt.Printf("%s\n",verr.Error())
 	}
+
+	end := time.Now()
+	fmt.Printf("Total: %v\n",end.Sub(start))
 }
